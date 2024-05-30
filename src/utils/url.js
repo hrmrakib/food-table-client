1facbf3aa4e9f1ae4e12764f4718b391b077ab89
hrmrakib <hrmrakibs@gmail.com>
2024-05-30 18:21:36 +0600

    add a new feature that logged-in user give a review in which store on database

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,3 @@
-export const baseURL = "http://localhost:5000";
+// export const baseURL = "http://localhost:5000";
 
-// export const baseURL = "https://food-table-server.vercel.app";
+export const baseURL = "https://food-table-server.vercel.app";
